a55e4c2c728685a1fb0fefc627e055eac17983c0
AnanyaGireesh <ananyaeg931@gmail.com>
2024-10-06 23:18:52 +0530

    Update home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,33 +7,88 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          "WEATHER",
-          style: TextStyle(
-            color: Colors.black,
-            fontSize:18,
-            fontWeight: FontWeight.bold
-
+      appBar: appBar(),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 40, left: 20, right:20),
+            decoration: BoxDecoration(
+              boxShadow: [
+                 BoxShadow(
+                color: Color(0xff101617).withOpacity(0.11),
+                blurRadius: 40,
+                spreadRadius: 0.0
+              )
+              ]
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white
+              ),
+            ),
           ),
-        ),
-        backgroundColor: Colors.white,
-        elevation:0,
-        leading: Container(
-           margin:EdgeInsets.all(10) ,
-           child: SvgPicture.asset(
-            'assets/icons/right.svg',
-            height: 20,
-            width: 20,
-           ),
-          decoration: BoxDecoration(
-            color: Color(0xffF7F8F8),
-            borderRadius: BorderRadius.circular(10)
-          ),
-        ),
-       
+        ],
       ),
     );
   }
+
+  AppBar appBar() {
+    return AppBar(
+      centerTitle: true,
+      title: Text(
+        "Weather",
+        style: TextStyle(
+          color: Colors.black,
+          fontSize:18,
+          fontWeight: FontWeight.bold
+
+        ),
+      ),
+      backgroundColor: Colors.white,
+      elevation:0.0,
+      leading: GestureDetector(
+        onTap: () {
+          
+        },
+        child: Container(
+           margin:EdgeInsets.all(10) ,
+         alignment: Alignment.center,
+         child: SvgPicture.asset(
+          'assets/icons/right.svg',
+          height: 20,
+          width: 20,
+         ),
+        decoration: BoxDecoration(
+          color: Color(0xffF7F8F8),
+          borderRadius: BorderRadius.circular(10)
+        ),
+      ),
+
+        
+      actions: [
+        GestureDetector(
+          onTap: () {
+            
+          },
+          child: Container(
+         margin:EdgeInsets.all(10) ,
+         alignment: Alignment.center,
+         child: SvgPicture.asset(
+          'assets/icons/more.svg',
+          height: 30,
+          width: 30,
+         ),
+        decoration: BoxDecoration(
+          color: Color(0xffF7F8F8),
+          borderRadius: BorderRadius.circular(10)
+        ),
+          ),
+          
+        ),
+      ],
+    
+          
+    ),
+    );
 }
